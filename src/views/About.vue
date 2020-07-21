e8e5b4ebcfdb8309f70f184a993ff791878c19a5
DanielHall-Eardley <350chevy8@gmail.com>
2020-07-21 17:36:43 -0400

    Modified 'About' paragraph

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <main class="about">
     <h1 class='default-h1'><span>About me</span></h1>
     <!-- <img src="" alt=""> -->
-    <p>I have been building applications for over two years. I have always enjoyed the act of creation and learning new skills as a way to increase the amount of different things I can build. I'm hardworking and a logical thinker that enjoys being challenged. I'm Australian by nationality, but I have been residing in Canada for the past three years.</p>
+    <p>I have been building applications for over two years. I have always enjoyed the act of creation and learning new skills as a way to increase the amount of different things I can build. I'm hardworking and a logical thinker that enjoys being challenged. I'm from Australia originally, but I have recently become a permanent resident of Canada.</p>
   </main>
 </template>
 
